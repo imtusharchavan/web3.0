4baef54d6acca43f4e12c3b9f5e240133df22410
imtusharchavan <tusharrchavan014@gmail.com>
2022-07-09 12:39:25 +0530

    Initial commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x2Df66617c45Ec9fda7269dB54DcA9fA789De1c9e';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,4 +3,13 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/Y4AraQANe6GpwOwym3XqNEUqvMJ6gESL',
+      accounts: [ '82410495b55ee27218f19bb3d8a00b9088dcec9ce51a11ca371060c3114dafbe' ]
+    }
+  }
 };
+
+
+// https://eth-goerli.g.alchemy.com/v2/Y4AraQANe6GpwOwym3XqNEUqvMJ6gESL
